b409670b315cae038e2c4663784ccac9efed8afd
Nikita Kalmychin <nikitakalmycin@gmail.com>
2025-11-03 22:39:23 +0300

    migrate to SOLID + Clean Architecture

--- src/main/kotlin/ru/driics/aitrade/application/usecase/BuildPromptUseCase.kt
+++ src/main/kotlin/ru/driics/aitrade/application/usecase/BuildPromptUseCase.kt
@@ -2,12 +2,12 @@ package ru.driics.aitrade.application.usecase
 
 import ru.driics.aitrade.domain.ports.MarketDataPort
 import ru.driics.aitrade.domain.ports.PromptOutputPort
+import ru.driics.aitrade.domain.services.PromptBuilder
 import ru.driics.aitrade.model.MarketState
 import ru.driics.aitrade.service.PromptBuilderService
 
 class BuildPromptUseCase(
     private val market: MarketDataPort,
-    private val builder: PromptBuilderService,
     private val outputPort: PromptOutputPort
 ) {
     suspend fun execute(
@@ -16,7 +16,7 @@ class BuildPromptUseCase(
         invocation: Long
     ): String {
         val state: MarketState = market.loadMarketState(symbols)
-        val prompt = builder.buildPrompt(state, sessionStartMs, invocation)
+        val prompt = PromptBuilder.build(state, sessionStartMs, invocation)
         outputPort.write(prompt)
         outputPort.print(prompt)
         return prompt
